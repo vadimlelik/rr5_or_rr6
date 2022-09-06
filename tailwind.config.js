4d0fd2f9c12aa239572273e6d73e74d063a131ac
vadim.cvirko <promiti90@gmail.com>
2022-09-06 10:51:03 +0300

    initial project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+	darkMode: "class",
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {},
+		screens: {
+			xs: "425px",
+			...defaultTheme.screens,
+		},
+	},
+	plugins: [
+		require("@tailwindcss/line-clamp"),
+		require("@tailwindcss/typography"),
+	],
+};
